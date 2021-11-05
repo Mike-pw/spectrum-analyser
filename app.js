9e23221c8814b7161935dec3246087bf908d7e82
Mike Melgren <mikemelgren@gmail.com>
2021-11-05 01:10:39 -0500

    note frequency object, slice data for large sample

--- app.js
+++ app.js
@@ -10,11 +10,9 @@ options.addEventListener("submit", function (e) {
 
   let analyser = audioCtx.createAnalyser();
 
-  //Sets the size of FFT window
-  let options = document.querySelector("#options")
-
   e.preventDefault()
 
+  //set the size of FFT window
   let fftSize = document.querySelector("#fftSize").value
   analyser.fftSize = fftSize
 
@@ -63,9 +61,17 @@ options.addEventListener("submit", function (e) {
 
       //convert into array
       let data = Array.from(dataArray)
-      let maxFrequency = sampleRate / 2
 
-      //// only want 6000Hz or MAYBE 8000Hz so slice off array iff too long
+      //calculate how many data points will be over 6000Hz
+      let maxFrequency = sampleRate / 2
+      let sliceFrequency = maxFrequency / data.length
+      let extraData = (maxFrequency - 6000) / sliceFrequency
+
+      //High pass filter to remove low frequencies
+      let highPass = Math.round(50 / sliceFrequency)
+
+      //slice array to remove from highpass to 6000Hz
+      data = data.slice(highPass, data.length - extraData)
 
       //normalize data
       normalizeData(data)
@@ -84,9 +90,10 @@ options.addEventListener("submit", function (e) {
         function minMaxScaling(element, index, arr) {
           arr[index] = (((element - min) / (max - min)) * height) - (height / 2);
         }
+
       }
 
-      document.querySelector("#frequency").textContent = data.indexOf((Math.max(...data))) * (sampleRate / data.length)
+      document.querySelector("#frequency").textContent = (data.indexOf(Math.max(...data)) + highPass) * sliceFrequency
 
       let xDistance = (1 / (data.length - 1)) * width;
 
@@ -109,5 +116,20 @@ options.addEventListener("submit", function (e) {
 
 });
 
+//Note frequencies
+const note = {
+  C: [16.35, 32.70, 65.41, 130.81, 261.63, 523.25, 1046.50, 2093.00, 4186.01],
+  CD: [17.32, 34.65, 69.30, 138.59, 277.18, 554.37, 1108.73, 2217.46, 4434.92],
+  D: [18.35, 36.71, 73.42, 146.83, 293.66, 587.33, 1174.66, 2349.32, 4698.63],
+  DE: [19.45, 38.89, 77.78, 155.56, 311.13, 622.25, 1244.51, 2489.02, 4978.03],
+  E: [20.60, 41.20, 82.41, 164.81, 329.63, 659.25, 1318.51, 2637.02, 5274.04],
+  F: [21.83, 43.65, 87.31, 174.61, 349.23, 698.46, 1396.91, 2793.83, 5587.65],
+  FG: [23.12, 46.25, 92.50, 185.00, 369.99, 739.99, 1479.98, 2959.96, 5919.91],
+  G: [24.50, 49.00, 98.00, 196.00, 392.00, 783.99, 1567.98, 3135.96, 6271.93],
+  GA: [25.96, 51.91, 103.83, 207.65, 415.30, 830.61, 1661.22, 3322.44, 6644.88],
+  A: [27.50, 55.00, 110.00, 220.00, 440.00, 880.00, 1760.00, 3520.00, 7040.00],
+  AB: [29.14, 58.27, 116.54, 233.08, 466.16, 932.33, 1864.66, 3729.31, 7458.62],
+  B: [30.87, 61.74, 123.47, 246.94, 493.88, 987.77, 1975.53, 3951.07, 7902.13]
+}
 
 
